c384ecb6db52c87e4bfed85bcac7be1455544412
Frances Fabian <francesfabian@Francess-MacBook-Pro.local>
2021-09-29 23:19:27 -0400

    modified App.js to say Frances Fabian

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         <code>My name is Frances Fabian and I'm going to master React!</code> 
         </p>
         <a
           className="App-link"
