dd23189756973bfe970ff9d5a56ff0d153a6b8e3
Frances Fabian <francesfabian@Francess-MacBook-Pro.local>
2021-09-29 23:50:43 -0400

    final commit

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href= "https://github.com/imustbefrances"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Heyy Check Me Out!
         </a>
       </header>
     </div>
