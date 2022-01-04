188be17820c1a3411e687a9ef1d9b5d8d60bf978
Frances Fabian <francesfabian@Francess-MBP.lan>
2022-01-03 22:12:24 -0500

    add AboutFrances

--- src/components/AboutFrances.js
+++ src/components/AboutFrances.js
@@ -8,6 +8,9 @@ export default class AboutMe extends Component{
                     <div className="three colums">  
                     <h2> About Frances </h2>
                     <img className= "profile-pic"  src= "Frances.jpg" alt= "Me" />
+                    <h4>I'm currently a Lehman College student. </h4>
+                    <h4>I'm a huge tomboy when it comes to sports especially basketball.</h4>
+                    <h4>Favorite player: Stephen Curry</h4>
                     </div>
                 </div>
             </section>
